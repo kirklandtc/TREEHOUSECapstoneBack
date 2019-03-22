d9aae77b734df86c8d2362be6a3751590f3c86d3
Thomas Kirkland <thomasckirkland@gmail.com>
2019-03-22 11:15:30 -0600

    updated routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../knex')
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  // res.render('index', { title: 'Express' });
+  return knex('reviews')
+  .then( data =>{
+    res.status(200).send(data)
+
+  })
+});
 
 
 module.exports = router;
