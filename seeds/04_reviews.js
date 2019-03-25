034242b4bae8bb4802ec55ad908837fd75897b64
Thomas Kirkland <thomasckirkland@gmail.com>
2019-03-25 16:26:47 -0600

    updates 25 Mar

--- seeds/04_reviews.js
+++ seeds/04_reviews.js
@@ -4,9 +4,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('reviews').insert([
-        { id: 1, name: 'The Vanderbilt is Incredible!', body:'This is the best treehouse I have ever owned!', user_id:1, treehouse_id:1},
+        { id: 1, name: 'The Vanderbilt is Incredible!', body:'This is the best treehouse I have ever owned! Truly an amazing finished mini-house that our children love!', user_id:1, treehouse_id:1},
         { id: 2, name: 'Great for the family, I recommend the Sierra', body:'Fantastic- my child loves it (I use it on the weekends)!', user_id:2, treehouse_id:2},
         { id: 3, name: 'The Commander has made my yard complete. Thanks Treehouse Boulder!', user_id:3, body:'Worth every penny -and then some! So many complements from the neighbors!', treehouse_id:3}
+
       ])
       .then(() => {
         return knex.raw("SELECT setval('reviews_id_seq', (SELECT MAX(id) FROM reviews))")
